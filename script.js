1de4959bd6cf485a1959719d9513c21cd8c8fec2
PadoleKiran <124488404+PadoleKiran@users.noreply.github.com>
2025-10-27 20:28:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+let output = document.getElementById("output");
+
+function pro() {
+	return new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			output.textContent = "Hello, world!";
+			resolve("Done");
+		},1000)
+	})
+}
+pro();
